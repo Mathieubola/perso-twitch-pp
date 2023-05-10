94e446a6aaca37f85ae317e0412484a7e737d545
Mathieubl <mathieulassurguere@hotmail.fr>
2023-05-10 23:26:12 +0200

    Overlay !

--- app.py
+++ app.py
@@ -25,7 +25,6 @@ app.register_blueprint(twitch_blueprint, url_prefix="/login")
 # Index endpoint
 @app.route('/')
 def index():
-    print("Index:", twitch.authorized)
     if not twitch.authorized:
         return render_template("index.html", url=url_for("twitch.login"))
     else:
@@ -34,47 +33,45 @@ def index():
 # Image upload endpoint
 @app.route('/choose', methods=['GET', 'POST'])
 def choose_image():
-    print("Choose:", twitch.authorized)
     if not twitch.authorized:
         return redirect(url_for("twitch.login"))
     else:
         user_info = twitch.get("/helix/users").json()
         twitch_username = user_info["data"][0]["login"].lower()
+
+        choices = listdir(duck_folder)
+        choices = [
+            path.join(duck_folder, choice)
+            for choice in choices
+            if choice.endswith(".png") or choice.endswith(".jpg")]
+
         if request.method == 'GET':
-            choices = listdir(duck_folder)
-            choices = [
-                path.join(duck_folder, choice)
-                for choice in choices
-                if choice.endswith(".png") or choice.endswith(".jpg")]
             return render_template("choose.html", images=choices, username=twitch_username)
         else:
             choice = request.form.get('canard', -1)
-            print("Choice:", choice)
 
             if choice == -1:
                 return "No image selected " + str(request.form)
 
             with open("./data/choices.json", 'r') as f:
-                choices = json.load(f)
+                datas_tream = json.load(f)
 
-            choices[twitch_username] = choice
+            datas_tream[twitch_username] = choice
 
             with open("./data/choices.json", 'w') as f:
-                json.dump(choices, f)
+                json.dump(datas_tream, f)
 
-            return "Image selected"
+            return render_template("choose.html", images=choices, username=twitch_username, message="Image choisie !")
 
 # Image retrieval endpoint
-@app.route('/img/<twitch_username>/<idk>')
+@app.route('/img/<twitch_username>/<idc>')
 @app.route('/img/<twitch_username>')
-def get_image(twitch_username, idk=None):
+def get_image(twitch_username, idc=None):
     with open("./data/choices.json", 'r') as f:
         choices = json.load(f)
-
-    print(twitch_username)
 
     choice = choices.get(twitch_username.lower(), path.join(duck_folder, "Sans_titre.jpg"))
     return send_file(choice)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host="0.0.0.0")
+    app.run(port=5000, host="0.0.0.0")
